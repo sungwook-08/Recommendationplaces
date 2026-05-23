946a4d2fe97c71fb12e0f5c1f69d333087fb87bd
sungwook-08 <ukbro08@naver.com>
2026-05-23 09:24:53 +0900

    Update and rename pbl33.py to pbl2/pbl/pbl2(menu).py

--- pbl2/pbl/pbl2(menu).py
+++ pbl2/pbl/pbl2(menu).py
@@ -17,7 +17,7 @@ def print_table(table, table_name):
     else:
         st.warning("출력할 장소가 없습니다")
 
-def get_user_input():
+def get_user_input(df):
     selected_region = st.selectbox("지역을 선택하세요", df["지역"].unique())
      selected_budget = st.number_input("사용 가능한 예산을 입력하세요", min_value=0, value=10000, step=1000)
     result = df[
